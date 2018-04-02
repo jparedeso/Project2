c2515db59a2386e74ba8ac72756c03fbda39d485
FuzzyJones <donald.jones83@gmail.com>
2018-04-02 19:59:46 -0400

    server listening, server routes commented out for now

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ app.use(express.static("app/public"));
 
 // Routes
 // =============================================================
-require("./app/routes/api-routes.js")(app);
-
-// Here we introduce HTML routing to serve different HTML files
-require("./app/routes/html-routes.js")(app);
+// require("./app/routes/api-routes.js")(app);
+// require("./app/routes/html-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
