21719c419f30a7a79d06b5038e228b6d0bd65f61
jparedeso <jparedeso@gmail.com>
2018-04-08 12:27:54 -0400

    Index.html content moved to handlebars files in views folder.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var index = require("./routes/index")
+var expressHbs = require("express-handlebars");
 
 // Sets up the Express App
 // =============================================================
@@ -10,14 +12,22 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
 
+//engine setup
+app.engine('.hbs', expressHbs({
+    defaultLayout: "layout",
+    extname: ".hbs"
+}));
+app.set("view engine", "hbs");
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
 // Static directory to be served
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("public"));
-
+app.use('/', index);
 
 // Routes
 // =============================================================
