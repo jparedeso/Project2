9179ed9bb33bdc33d07aa7e731f6eb8a117c33a8
Brian Sandoval <jbriansandoval@gmail.com>
2018-04-09 20:12:34 -0400

    header

--- routes/web/books.js
+++ routes/web/books.js
@@ -6,5 +6,7 @@ var router = express.Router();
 router.get('/postbooks', function(req, res, next) {
     res.render('books/postbooks', {title: 'Biblioteca Boyz'});
 });
-
+router.get('/mybooks', function(req, res, next) {
+    res.render('books/mybooks', {title: 'Biblioteca Boyz'});
+});
 module.exports = router;
